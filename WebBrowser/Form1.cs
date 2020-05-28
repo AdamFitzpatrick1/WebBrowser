d6500f1a940adf33eb26fc668bbeeff73a097eeb
adamfitzpatrick2020@outlook.com <adamfitzpatrick2020@outlook.com>
2020-05-28 14:21:11 +0100

    Updated

---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string webPage = textBox1.Text.Trim();
+            webBrowser1.Navigate(webPage);
+        }
+    }
+}
